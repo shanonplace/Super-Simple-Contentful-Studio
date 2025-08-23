72ed0c8a7e474a3d4816887ccbf6bc3548f7797d
Shanon Place <shanonplace@gmail.com>
2025-08-22 17:00:36 -0700

    Updated to the latest package and add added a boundary

--- src/app/studio-config.ts
+++ src/app/studio-config.ts
@@ -11,70 +11,73 @@ import { Wistia } from "./components/Wistia";
 
 //defineComponents([]);
 
+console.log(
+  `Registering Defined components: ${Button.name}, ${InfoCard.name}, ${Wistia.name}`
+);
 // Uncomment the following code to define custom components
-// defineComponents([
-//   {
-//     component: Button,
-//     definition: {
-//       id: "button",
-//       name: "My Custom Button",
-//       category: "Custom Components",
-//       variables: {
-//         text: {
-//           displayName: "Text",
-//           type: "Text",
-//           defaultValue: "Click me",
-//         },
-//       },
-//     },
-//   },
-//   {
-//     component: InfoCard,
-//     definition: {
-//       id: "info-card",
-//       name: "Info Card",
-//       category: "Custom Components",
-//       variables: {
-//         title: {
-//           displayName: "Title",
-//           type: "Text",
-//           defaultValue: "Default title",
-//         },
-//         description: {
-//           displayName: "Description",
-//           type: "Text",
-//           defaultValue: "Default description",
-//         },
-//         image: {
-//           displayName: "Background Image",
-//           type: "Media",
-//           defaultValue:
-//             "https://images.ctfassets.net/z9lgl0yt90y7/24V3RUrw0JaqTqA8FBcp2F/e93aa6b8e94614db75689fb6892c7ac2/Ohio_First_Lady_Logo.webp",
-//         },
-//         linkUrl: {
-//           displayName: "Link URL",
-//           type: "Text",
-//           defaultValue: "https://firstlady.ohio.gov/",
-//         },
-//       },
-//     },
-//   },
-//   {
-//     component: Wistia,
-//     definition: {
-//       id: "wistia",
-//       name: "Wistia Video",
-//       category: "Custom Components",
-//       variables: {
-//         videoId: {
-//           displayName: "Video ID",
-//           type: "Text",
-//           defaultValue: "nuhuqj4dg0",
-//         },
-//       },
-//     },
-//   },
-// ]);
+defineComponents([
+  {
+    component: Button,
+    definition: {
+      id: "button",
+      name: "My Custom Button",
+      category: "Custom Components",
+      variables: {
+        text: {
+          displayName: "Text",
+          type: "Text",
+          defaultValue: "Click me",
+        },
+      },
+    },
+  },
+  {
+    component: InfoCard,
+    definition: {
+      id: "info-card",
+      name: "Info Card",
+      category: "Custom Components",
+      variables: {
+        title: {
+          displayName: "Title",
+          type: "Text",
+          defaultValue: "Shanon Rocks",
+        },
+        description: {
+          displayName: "Description",
+          type: "Text",
+          defaultValue: "Default description",
+        },
+        image: {
+          displayName: "Background Image",
+          type: "Media",
+          defaultValue:
+            "https://images.ctfassets.net/z9lgl0yt90y7/24V3RUrw0JaqTqA8FBcp2F/e93aa6b8e94614db75689fb6892c7ac2/Ohio_First_Lady_Logo.webp",
+        },
+        linkUrl: {
+          displayName: "Link URL",
+          type: "Text",
+          defaultValue: "https://firstlady.ohio.gov/",
+        },
+      },
+    },
+  },
+  {
+    component: Wistia,
+    definition: {
+      id: "wistia",
+      name: "Wistia Video",
+      category: "Custom Components",
+      variables: {
+        videoId: {
+          displayName: "Video ID",
+          type: "Text",
+          defaultValue: "nuhuqj4dg0",
+        },
+      },
+    },
+  },
+]);
 
 defineBreakpoints([]);
 
@@ -103,17 +106,17 @@ defineBreakpoints([]);
 //   },
 // ]);
 
-defineDesignTokens([]);
+//defineDesignTokens([]);
 
 // Uncomment the following code to define custom design tokens
-// defineDesignTokens({
-//   spacing: { XS: "4px", S: "16px", M: "32px", L: "64px", XL: "128px" },
-//   sizing: { XS: "16px", S: "100px", M: "300px", L: "600px", XL: "1024px" },
-//   color: {
-//     Slate: "#94a3b8",
-//     Azure: "azure",
-//     Orange: "#fdba74",
-//     Blue: "#0000ff",
-//   },
-//   textColor: { Dark: "#1a1a1a", Light: "#efefef", Slate: "#94a3b8" },
-// });
+defineDesignTokens({
+  spacing: { XS: "4px", S: "16px", M: "32px", L: "64px", XL: "128px" },
+  sizing: { XS: "16px", S: "100px", M: "300px", L: "600px", XL: "1024px" },
+  color: {
+    Slate: "#94a3b8",
+    Azure: "azure",
+    Orange: "#fdba74",
+    Blue: "#0000ff",
+  },
+  textColor: { Dark: "#1a1a1a", Light: "#efefef", Slate: "#94a3b8" },
+});
